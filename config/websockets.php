664f52888cf48610cb1f022545989467a9b28a47
Ollie Read <ollie@ollieslab.io>
2019-07-21 02:20:03 +0100

    Update SSL config for master

--- config/websockets.php
+++ config/websockets.php
@@ -98,13 +98,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => null,
+        'local_cert' => '/etc/letsencrypt/live/arc.ollieread.dev/fullchain.pem',
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk'   => null,
+        'local_pk'   => 'etc/letsencrypt/live/arc.ollieread.dev/privkey.pem',
 
         /*
          * Passphrase for your local_cert file.
